13c17859bd9cde224f95ed238afcf5b1712cbc0a
maharib820 <maharib820@gmail.com>
2023-10-20 17:50:42 +0600

    partigular-get-using-product-implemented

--- index.js
+++ index.js
@@ -48,6 +48,15 @@ async function run() {
             res.send(result)
         })
 
+        // get added product
+        app.get("/products/:product", async (req, res) => {
+            const product = req.params.product;
+            const filter = { product: product }
+            const cursor = productCollection.find(filter);
+            const result = await cursor.toArray();
+            res.send(result)
+        })
+
         // Send a ping to confirm a successful connection
         await client.db("admin").command({ ping: 1 });
         console.log("Pinged your deployment. You successfully connected to MongoDB!");
