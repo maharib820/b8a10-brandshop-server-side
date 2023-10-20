20c2725a18656b95f9d96867e856494039d6f7c9
maharib820 <maharib820@gmail.com>
2023-10-20 09:56:55 +0600

    data-send-to-database-implemented

--- index.js
+++ index.js
@@ -5,6 +5,9 @@ const { MongoClient, ServerApiVersion } = require('mongodb');
 const app = express();
 const port = process.env.PORT || 5000;
 
+app.use(cors());
+app.use(express.json());
+
 // ............................................................................................................................................................
 // Mongo start.................................................................................................................................................
 const uri = "mongodb+srv://r_and_f_admin:helloworldadmin@cluster0.8ydx2m5.mongodb.net/?retryWrites=true&w=majority";
@@ -22,12 +25,25 @@ async function run() {
     try {
         // Connect the client to the server	(optional starting in v4.7)
         await client.connect();
+
+        const productCollection = client.db("randf").collection("products");
+
+        // CRUD operation section
+
+        // POST from add product
+        app.post("/addnewproduct/", async (req, res) => {
+            const getProduct = req.body;
+            console.log(getProduct);
+            const result = await productCollection.insertOne(getProduct);
+            res.send(result)
+        })
+
         // Send a ping to confirm a successful connection
         await client.db("admin").command({ ping: 1 });
         console.log("Pinged your deployment. You successfully connected to MongoDB!");
     } finally {
         // Ensures that the client will close when you finish/error
-        await client.close();
+        // await client.close();
     }
 }
 run().catch(console.dir);
