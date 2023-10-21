80ebedfd478b4fc49e288a636ad385c4ac93f169
maharib820 <maharib820@gmail.com>
2023-10-21 18:11:34 +0600

    final-push

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const cors = require('cors');
+require('dotenv').config();
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 
 const app = express();
@@ -8,9 +9,12 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
+console.log(process.env.DB_USER);
+console.log(process.env.DB_PASS);
+
 // ............................................................................................................................................................
 // Mongo start.................................................................................................................................................
-const uri = "mongodb+srv://r_and_f_admin:helloworldadmin@cluster0.8ydx2m5.mongodb.net/?retryWrites=true&w=majority";
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.8ydx2m5.mongodb.net/?retryWrites=true&w=majority`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
